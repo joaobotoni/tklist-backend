26d875ecfe9d0d77c8a66d2cfe35050564611cb8
joaobotoni <joaobotoni@hotmail.com>
2024-11-11 09:01:07 -0400

    first commit

---
+++ src/main/java/com/example/tklist/TklistApplication.java
@@ -0,0 +1,13 @@
+package com.example.tklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tklist/TklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
