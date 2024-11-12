acddddc1cd7117d3fe43e524e364a2f14de4ee8e
joaobotoni <joaobotoni@hotmail.com>
2024-11-12 10:43:38 -0400

    update

--- src/main/resources/db/migration/V1_create-table-tasks.sql
+++ src/main/resources/db/migration/V1_create-table-tasks.sql
@@ -1,10 +1,22 @@
-CREATE TABLE tasks(
-    id serial PRIMARY KEY,
-    title varchar(100) NOT NULL,
-    assignee varchar(100) NOT NULL UNIQUE,
-    description varchar(200) NOT NULL,
-    priority smallint CHECK (priority IN (1, 2, 3)) NOT NULL,
-    createdAt timestamp DEFAULT current_timestamp,
-    dueDate timestamp NOT NULL
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    assignee VARCHAR(100) NOT NULL UNIQUE,
+    createdAt TIMESTAMP DEFAULT NOW(),
+    description VARCHAR(200) NOT NULL,
+    priority SMALLINT CHECK (priority IN (0, 1, 2)) NOT NULL,
+    dueDate TIMESTAMP NOT NULL
 );
 
+CREATE TABLE task_list (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE belong (
+    task_id INT NOT NULL,
+    list_id INT NOT NULL,
+    position_task INT NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
+    FOREIGN KEY (list_id) REFERENCES task_list(id) ON DELETE CASCADE
+);
